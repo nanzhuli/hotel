2a241ec5276273c44fa71cd48be3cec91bc5784b
lnz <957039113@qq.com>
2018-11-19 19:07:10 +0800

    hotel_demo project you all can have a look

---
+++ HotelManage/src/main/java/com/hotel/HotelApplication.java
@@ -0,0 +1,14 @@
+package com.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HotelApplication.class, args);
+		System.out.println("hello");
+	}
+}
---
+++ HotelManage/src/main/java/com/hotel/controller/hotelController.java
@@ -0,0 +1,15 @@
+package com.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotelController {
+    @RequestMapping(value = "/hotel",method = RequestMethod.GET)
+    public String hotel(){
+        return "hotel";
+    }
+
+}
