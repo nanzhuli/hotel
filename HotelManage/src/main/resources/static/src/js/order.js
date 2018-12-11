26879271c0599d3cc970d66446f93678e46c6e72
zhyib <33754011+zhyib@users.noreply.github.com>
2018-12-11 19:40:16 +0800

    order做了一半

--- HotelManage/src/main/resources/static/src/js/order.js
+++ HotelManage/src/main/resources/static/src/js/order.js
@@ -148,7 +148,7 @@ var order = {
                             allRoomData.forEach(function (each) {
                                 // 插一行房间信息和查看按钮
                                 var str = "<tr><td>" + each.roomno + "</td><td>" + home.funcs.spaceFunc(each.brand) + "</td>" +
-                                    "<td><a href='#' class='rooms' id='room-" + each.roomno + "'><i class=\"layui-icon layui-icon-list\"></i></a></td>";
+                                    "<td><a href='#' class='rooms' id='room-" + each.roomno + "'><i class=\"layui-icon layui-icon-list\"></i></a></td></tr>";
                                 order.funcs.detailHandler(str);
                                 // 绑定查看按钮事件
                                 var roomBtns = $(".rooms");
@@ -160,26 +160,28 @@ var order = {
                             type: 1,
                             title: '查看',
                             content:
-                                "<div id='orderDetail' class='popup'>" +
-                                "<table class='pop-table' border='2px'>" +
-                                "<tr><td>订单号</td><td>" + (res.orderno) + "</td></tr>" +
-                                "<tr><td>房间数量</td><td>" + (res.roomcount) + "</td></tr>" +
-                                "<tr><td>订单价格</td><td>" + (res.price) + "</td></tr>" +
-                                "<tr><td>姓名</td><td>" + (res.name) + "</td></tr>" +
-                                "<tr><td>身份证号</td><td>" + (res.id) + "</td></tr>" +
-                                "<tr><td>联系方式</td><td>" + (res.phone) + "</td></tr>" +
-                                "<tr><td>入住时间</td><td>" + home.funcs.timeStrDate(res.starttime) + "</td></tr>" +
-                                "<tr><td>离开时间</td><td>" + home.funcs.timeStrDate(res.endtime) + "</td></tr>" +
-                                "<tr><td>是否会员</td><td>" + home.vars.member[res.ismenber] + "</td></tr>" +
-                                "<tr><td>是否入住</td><td>" + home.vars.enter[res.isenter] + "</td></tr>" +
-                                "</table>" +
-                                "<table id='detail-table' class='pop-table' border='2px'>" +
+                                "<div id='pop-div-room' class='display-table'>" +
+                                "<table id='detail-table' class='layui-table' border='1px'>" +
+                                "<tr><th colspan='3'>订单详情</th></tr>" +
+                                "<tr><td>订单号</td><td colspan='2'>" + (res.orderno) + "</td></tr>" +
+                                "<tr><td>房间数量</td><td colspan='2'>" + (res.roomcount) + "</td></tr>" +
+                                "<tr><td>订单价格</td><td colspan='2'>" + (res.price) + "</td></tr>" +
+                                "<tr><td>姓名</td><td colspan='2'>" + (res.name) + "</td></tr>" +
+                                "<tr><td>身份证号</td><td colspan='2'>" + (res.id) + "</td></tr>" +
+                                "<tr><td>联系方式</td><td colspan='2'>" + (res.phone) + "</td></tr>" +
+                                "<tr><td>入住时间</td><td colspan='2'>" + home.funcs.timeStrDate(res.starttime) + "</td></tr>" +
+                                "<tr><td>离开时间</td><td colspan='2'>" + home.funcs.timeStrDate(res.endtime) + "</td></tr>" +
+                                "<tr><td>是否会员</td><td colspan='2'>" + home.vars.member[res.ismenber] + "</td></tr>" +
+                                "<tr><td>是否入住</td><td colspan='2'>" + home.vars.enter[res.isenter] + "</td></tr>" +
+                                "<tr><th colspan='3'>房间详情</th></tr>" +
                                 "<tr><td>房间号</td><td>绑定车牌</td><td>查看房间信息</td></tr>" +
                                 "</table>" +
                                 "</div>",
-                            area: ['400px', '600px'],
+                            area: ['600px', '600px'],
                             btn: ['确认'],
-                            offset: ['5%', '35%'],
+                            offset: 'auto',// ['5%', '35%'],
+                            zIndex: 5,
+                            btnAlign: 'c',
                             yes: function (index) {
                                 layer.close(index)
                             }
@@ -194,60 +196,117 @@ var order = {
                 console.log("EDIT");
                 var code = this.id.substr(5);
                 console.log(code);
+                // 获取订单信息
                 $.get(home.urls.order.getOne + code, {}, function (result) {
                     console.log(result);
                     var res = result.data;
                     layui.use('layer', function () {
+                        // 使用订单号查找订单所包含的所有房间
+                        $.get(home.urls.room.getAll + res.orderno, {}, function (allRoom) {
+                            console.log(allRoom);
+                            var allRoomData = allRoom.data;
+                            allRoomData.forEach(function (each) {
+                                // 插一行房间信息和查看按钮
+                                var str = "<tr><td>" + each.roomno + "</td>" +
+                                    "<td><input type='text' style='width:66px' id='brand' value='" + home.funcs.spaceFunc(each.brand) + "'><button class=\"layui-btn\">保存</button></td>" +
+                                    "<td><a href='#' class='rooms' id='room-edit-" + each.roomno + "'><i class=\"layui-icon layui-icon-list\"></i></a></td>" +
+                                    "</tr>";
+                                order.funcs.detailHandler(str);
+                                // 绑定查看按钮事件
+                                var roomBtns = $(".rooms");
+                                order.funcs.bindRoomsEventListener(roomBtns);
+                            });
+                        });
+                        // console.log(str);
                         layer.open({
                             type: 1,
                             title: '编辑',
                             content:
-                                "<div id='addModal' class='popup'>" +
-                                "<p>订单编号：<input type='number' id='room-no' value='" + res.orderno + "' disabled='disabled'/></p>" +
-                                "<p>客房单价：<input type='number' id='room-price' value='" + res.price + "'/></p>" +
-                                "<p>客房备注：<input type='text' id='comment' value='" + home.funcs.spaceFuncEmpty(res.comment) + "'/></p>" +
+                                "<div id='pop-div-room' class='display-table'>" +
+                                "<table id='detail-table' class='layui-table' border='1px'>" +
+                                "<tr><th colspan='3'>订单详情</th></tr>" +
+                                "<tr><td>订单号</td><td colspan='2'>" + (res.orderno) + "</td></tr>" +
+                                "<tr><td>房间数量</td><td colspan='2'>" + (res.roomcount) + "</td></tr>" +
+                                "<tr><td>订单价格</td><td colspan='2'>" + (res.price) + "</td></tr>" +
+                                "<tr><td>姓名</td><td colspan='2'><input id='name' style='width: 145px' type='text' value='" + (res.name) + "'></td></tr>" +
+                                "<tr><td>身份证号</td><td colspan='2'><input id='id' style='width: 145px' type='number' value='" + (res.id) + "'></td></tr>" +
+                                "<tr><td>联系方式</td><td colspan='2'><input id='phone' style='width: 145px' type='number' value='" + (res.phone) + "'></td></tr>" +
+                                "<tr><td>入住时间</td><td colspan='2'>" + home.funcs.timeStrDate(res.starttime) + "</td></tr>" +
+                                "<tr><td>离开时间</td><td colspan='2'>" + home.funcs.timeStrDate(res.endtime) + "</td></tr>" +
+                                "<tr><td>是否会员</td><td colspan='2'>" + home.vars.member[res.ismenber] + "</td></tr>" +
+                                "<tr><td>是否入住</td><td colspan='2'>" + home.vars.enter[res.isenter] + "</td></tr>" +
+                                "<tr><th colspan='3'>房间详情</th></tr>" +
+                                "<tr><td>房间号</td><td>绑定车牌</td><td>编辑</td></tr>" +
+                                "</table>" +
                                 "</div>",
-                            area: ['350px', '380px'],
-                            btn: ['确认', '取消'],
-                            offset: ['30%', '35%'],
+                            area: ['600px', '600px'],
+                            btn: ['确认'],
+                            offset: 'auto',// ['5%', '35%'],
+                            zIndex: 5,
+                            btnAlign: 'c',
                             yes: function (index) {
-                                var roomno = $('#room-no').val();
-                                var type = $('#room-type').val();
-                                var price = $('#room-price').val();
-                                var ifwindow = $('#ifwindow').val();
-                                var comment = $('#comment').val();
-                                console.log(roomno);
-                                console.log(type);
-                                console.log(price);
-                                console.log(ifwindow);
-                                console.log(comment);
-                                $.post(home.urls.order.update + roomno, {
-                                    // roomno: roomno,
-                                    type: type,
-                                    price: price,
-                                    ifwindow: ifwindow,
-                                    comment: comment
-                                }, function (result) {
-                                    console.log(result);
-                                    layer.msg(result.msg, {
-                                        offset: ['50%', '50%'],
-                                        time: 700
-                                    });
-                                    if (result.code === 0) {
-                                        var time = setTimeout(function () {
-                                            order.init();
-                                            clearTimeout(time);
-                                        }, 500)
-                                    }
-                                    layer.close(index)
-                                })
-                            },
-                            btn2: function (index) {
                                 layer.close(index)
                             }
                         });
-                    })
+                    });
                 });
+
+                // var code = this.id.substr(5);
+                // console.log(code);
+                // $.get(home.urls.order.getOne + code, {}, function (result) {
+                //     console.log(result);
+                //     var res = result.data;
+                //     layui.use('layer', function () {
+                //         layer.open({
+                //             type: 1,
+                //             title: '编辑',
+                //             content:
+                //                 "<div id='addModal' class='popup'>" +
+                //                 "<p>订单编号：<input type='number' id='room-no' value='" + res.orderno + "' disabled='disabled'/></p>" +
+                //                 "<p>客房单价：<input type='number' id='room-price' value='" + res.price + "'/></p>" +
+                //                 "<p>客房备注：<input type='text' id='comment' value='" + home.funcs.spaceFuncEmpty(res.comment) + "'/></p>" +
+                //                 "</div>",
+                //             area: ['350px', '380px'],
+                //             btn: ['确认', '取消'],
+                //             offset: auto,//: ['30%', '35%'],
+                //             yes: function (index) {
+                //                 var roomno = $('#room-no').val();
+                //                 var type = $('#room-type').val();
+                //                 var price = $('#room-price').val();
+                //                 var ifwindow = $('#ifwindow').val();
+                //                 var comment = $('#comment').val();
+                //                 console.log(roomno);
+                //                 console.log(type);
+                //                 console.log(price);
+                //                 console.log(ifwindow);
+                //                 console.log(comment);
+                //                 $.post(home.urls.order.update + roomno, {
+                //                     // roomno: roomno,
+                //                     type: type,
+                //                     price: price,
+                //                     ifwindow: ifwindow,
+                //                     comment: comment
+                //                 }, function (result) {
+                //                     console.log(result);
+                //                     layer.msg(result.msg, {
+                //                         offset: ['50%', '50%'],
+                //                         time: 700
+                //                     });
+                //                     if (result.code === 0) {
+                //                         var time = setTimeout(function () {
+                //                             order.init();
+                //                             clearTimeout(time);
+                //                         }, 500)
+                //                     }
+                //                     layer.close(index)
+                //                 })
+                //             },
+                //             btn2: function (index) {
+                //                 layer.close(index)
+                //             }
+                //         });
+                //     })
+                // });
             })
         },
         bindDeleteEventListener: function (deleteBtns) {
@@ -263,7 +322,7 @@ var order = {
                         content: "<h5 style='text-align: center;padding-top: 8px'>确认要删除该记录？</h5>",
                         area: ['180px', '130px'],
                         btn: ['确认', '取消'],
-                        offset: ['35%', '40%'],
+                        offset: 'auto',//: ['35%', '40%'],
                         yes: function (index) {
                             console.log(code);
                             $.post(home.urls.order.delete + code, {}, function (result) {
@@ -319,7 +378,7 @@ var order = {
                         var allRoomIdData = allRoomId.data;
                         allRoomIdData.forEach(function (each) {
                             // 插一行用户信息
-                            var str = "<tr><td>" + each.roomno + "</td><td>" + each.name + "</td><td>" + each.id + "</td>";
+                            var str = "<tr><td>" + each.roomno + "</td><td>" + each.name + "</td><td>" + each.id + "</td></tr>";
                             order.funcs.detailRoomHandler(str);
                         });
                     });
@@ -327,13 +386,19 @@ var order = {
                         type: 1,
                         title: '房间' + code,
                         content:
-                            "<table id='room-detail-table' class='pop-table' border='2px'>" +
+                            "<div id='pop-div-roomid' class='display-table'>" +
+                            "<table id='room-detail-table' class='layui-table' border='1px' width='70%'>" +
                             "<tr><td>房间号</td><td>姓名</td><td>身份证号</td></tr>" +
                             "</table>" +
                             "</div>",
-                        area: ['400px', '600px'],
+                        area: ['500px', '400px'],
                         btn: ['确认'],
-                        offset: ['5%', '35%'],
+                        offset: 'auto', //['5%', '35%'],
+                        zIndex: 10,
+                        btnAlign: 'c',
+                        success: function (layero) {
+                            layer.setTop(layero); //重点2
+                        },
                         yes: function (index) {
                             layer.close(index)
                         }
